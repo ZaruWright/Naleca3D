b852b435af38153130575ee5c1943bdca2dfcaba
ZaruWright <samuel_gse@hotmail.com>
2018-10-06 12:31:47 +0200

    First commit! Added a simple class to debug

---
+++ Naleca3d/Assets/Tools/NalecaLog/NalecaDebug.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NalecaDebug : MonoBehaviour {
+
+	public enum Severity
+    {
+        Debug,
+        Warning,
+        Error
+    }
+
+    static public void Log(string message, Severity severity = Severity.Debug)
+    {
+        switch (severity)
+        {
+            case Severity.Debug:
+                Debug.Log("<color=black>Debug: " + message + "</color>");
+                break;
+            case Severity.Warning:
+                Debug.Log("<color=yellow>Warning: " + message + "</color>");
+                break;
+            case Severity.Error:
+                Debug.Log("<color=red>Error: " + message + "</color>");
+                break;
+        }
+    }
+}
